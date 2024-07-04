26aee4c2dd0dd66ae021bc309c20a4274e0ba95f
Anurag Rajput <86197651+anurag4667@users.noreply.github.com>
2024-07-04 10:04:21 +0530

    Update dijkstra.cpp

--- dijkstra.cpp
+++ dijkstra.cpp
@@ -1,17 +1,19 @@
-vector <int> dijkstra(int V, vector<vector<int>> adj[], int S)
+vector <long long> dijkstra(int V, vector<vector<int>> adj[], int S)
     {
-        vector<int> dist(V,INT_MAX);
+        vector<long long> dist(V,INT_MAX);
         dist[S] = 0;
         
-        priority_queue<pair<int,int>,vector<pair<int,int>>, greater<pair<int,int>>> pq;
+        priority_queue<pair<long long,long long>,vector<pair<long long,long long>>, greater<pair<long long,long long>>> pq;
         
         pq.push({0,S});
         
         while(!pq.empty()){
-            int node = pq.top().second;
-            int d = pq.top().first;
+            long long node = pq.top().second;
+            long long d = pq.top().first;
             
             pq.pop();
+
+            if(d > dist[node]) continue;
             
             for(auto N : adj[node]){
                 if(dist[N[0]] > N[1] + d){
